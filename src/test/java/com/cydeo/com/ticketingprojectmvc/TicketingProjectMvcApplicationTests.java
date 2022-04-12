68fc74291a111b8da6cc048e5b5a05449a043020
Filizsi <filizcoskunuzer@gmail.com>
2022-04-11 23:36:10 -0500

    Ticketing project for UI section

---
+++ src/test/java/com/cydeo/com/ticketingprojectmvc/TicketingProjectMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.com.ticketingprojectmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
